a51c54c1470ef8be76ee46d44b3717b70592c006
Peter D'Pong <peterdpong@gmail.com>
2023-02-04 01:08:11 -0500

    nextjs: fix build

---
+++ pages/notes/index.tsx
@@ -0,0 +1,47 @@
+import Head from "next/head";
+
+export default function NotesIndex() {
+  return (
+    <>
+      <Head>
+        <meta http-equiv="Content-Language" content="en" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="author" content="Peter Damrongpiriyapong" />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:site" content="@peterdpong" />
+        <meta property="twitter:title" content="Peter Damrongpiriyapong" />
+        <meta
+          property="twitter:image"
+          content="https://peterdpong.me/static/img/preview-img.png"
+        />
+        <meta
+          property="twitter:description"
+          content="Computer Engineering at the University of Toronto and an incoming Software Engineer at Figma."
+        />
+        <meta property="twitter:url" content="https://peterdpong.me" />
+
+        <meta name="title" content="Peter Damrongpiriyapong" />
+        <meta
+          property="og:image"
+          content="https://peterdpong.me/static/img/preview-img.png"
+        />
+        <meta
+          property="description"
+          content="Computer Engineering at the University of Toronto and an incoming Software Engineer at Figma."
+        />
+        <meta
+          property="og:description"
+          content="Computer Engineering at the University of Toronto and an incoming Software Engineer at Figma."
+        />
+        <meta property="og:url" content="https://peterdpong.me" />
+
+        <link rel="icon" href="./favicon.png" type="image/x-icon" />
+
+        <title>Peter Damrongpiriyapong</title>
+      </Head>
+    </>
+  );
+}
