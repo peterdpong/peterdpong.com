f4007b21895aea2777909e3121a0894d1fddbf9c
Peter Damrongpiriyapong <peterdpong@gmail.com>
2024-02-20 23:11:41 -0500

    Add hover cards on work page

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -4,9 +4,7 @@ import { Link } from "@/components/Link";
 import { Section } from "@/components/Section";
 import {
   Description,
-  ItemSubtitle,
   ItemTitle,
-  SectionTitle,
   Title,
 } from "@/components/Typography";
 import { ContentWrapper, PageWrapper } from "@/components/Wrappers";
@@ -83,16 +81,15 @@ export default function Index() {
           </Section>
           <Section>
             <FadeInWrapper css={{ "--delay-num": 2 }}>
-              <YearDiv>
-                <YearTitle>2023</YearTitle>
-              </YearDiv>
               <Card>
-                <ItemTitle>FigJam AI</ItemTitle>
-                <Description>
-                  Helped bring AI capabilities to FigJam. AI powered generation
-                  of templates and visuals, and summarization/clustering of
-                  stickies.
-                </Description>
+                <FloatingCardRight css={{ "--rotate-deg": "-5deg", "--rotate-deg-hover": "2deg" }}>
+                  <CardTitle>FigJam AI</CardTitle>
+                  <CardDescription>
+                    Helped bring AI capabilities to FigJam. AI powered generation
+                    of templates and visuals, and summarization/clustering of
+                    stickies.
+                  </CardDescription>
+                </FloatingCardRight>
                 <Image
                   src="/gif/figjam_ai.gif"
                   alt="Gif of FigJam AI"
@@ -101,39 +98,24 @@ export default function Index() {
                   sizes="100vw"
                   style={{
                     width: "100%",
-                    height: "350px",
+                    height: "400px",
                     objectFit: "cover",
                     borderRadius: "8px",
                   }}
                 />
-
-                {/* <Link href="work/figjam-ai">
-                  <ItemTitle>
-                    FigJam AI
-                    <Image
-                      src="img/nav-arrow.svg"
-                      alt="navigation-arrow"
-                      width={12}
-                      height={12}
-                    />
-                  </ItemTitle>
-                </Link> */}
               </Card>
-              <ItemSubtitle></ItemSubtitle>
             </FadeInWrapper>
           </Section>
           <Section>
             <FadeInWrapper css={{ "--delay-num": 2 }}>
-              <YearDiv>
-                <YearTitle>2022</YearTitle>
-              </YearDiv>
-              <Column>
                 <Card>
-                  <ItemTitle>Markdown in FigJam</ItemTitle>
-                  <Description>
+                  <FloatingCardLeft  css={{ "--rotate-deg": "4deg", "--rotate-deg-hover": "-2deg" }}>
+                    <CardTitle>Markdown in FigJam</CardTitle>
+                    <CardDescription>
                     Adding Markdown support for text, improving text formatting
-                    in FigJam.
-                  </Description>
+                      in FigJam.
+                    </CardDescription>
+                  </FloatingCardLeft>
                   <Image
                     src="/gif/figjam_markdown.gif"
                     alt="Gif of FigJam AI"
@@ -142,58 +124,38 @@ export default function Index() {
                     sizes="100vw"
                     style={{
                       width: "100%",
-                      height: "350px",
+                      height: "400px",
                       objectFit: "cover",
                       borderRadius: "8px",
                     }}
                   />
-
-                  {/* <Link href="/work/markdown">
-                    <ItemTitle>
-                      Markdown in FigJam
-                      <Image
-                        src="img/nav-arrow.svg"
-                        alt="navigation-arrow"
-                        width={12}
-                        height={12}
-                      />
-                    </ItemTitle>
-                  </Link> */}
                 </Card>
-
-                <Card>
-                  <ItemTitle>Quick Create Improvements</ItemTitle>
-                  <Description>
-                    Smart content aware quick create placement for a better
-                    diagramming experience in FigJam.
-                  </Description>
-                  <Image
-                    src="/gif/figjam_quickadd.gif"
-                    alt="Gif of FigJam AI"
-                    width="0"
-                    height="0"
-                    sizes="100vw"
-                    style={{
-                      width: "100%",
-                      height: "500px",
-                      objectFit: "cover",
-                      borderRadius: "8px",
-                    }}
-                  />
-
-                  {/* <Link href="/work/quick-create">
-                    <ItemTitle>
-                      Quick Create Improvements
-                      <Image
-                        src="img/nav-arrow.svg"
-                        alt="navigation-arrow"
-                        width={12}
-                        height={12}
-                      />
-                    </ItemTitle>
-                  </Link> */}
-                </Card>
-              </Column>
+            </FadeInWrapper>
+          </Section>
+          <Section>
+            <FadeInWrapper css={{ "--delay-num": 2 }}>
+              <Card>
+                <FloatingCardRight css={{ "--rotate-deg": "-4deg", "--rotate-deg-hover": "1deg" }}>
+                  <CardTitle>Quick Create Improvements</CardTitle>
+                  <CardDescription>
+                  Smart content aware quick create placement for a better
+                  diagramming experience in FigJam.
+                  </CardDescription>
+                </FloatingCardRight>
+                <Image
+                  src="/gif/figjam_quickadd.gif"
+                  alt="Gif of FigJam AI"
+                  width="0"
+                  height="0"
+                  sizes="100vw"
+                  style={{
+                    width: "100%",
+                    height: "400px",
+                    objectFit: "cover",
+                    borderRadius: "8px",
+                  }}
+                />
+              </Card>
             </FadeInWrapper>
           </Section>
         </ContentWrapper>
@@ -204,31 +166,67 @@ export default function Index() {
 }
 
 const Card = styled("div", {
-  display: "flex",
-  flexDirection: "column",
-  gap: "4px",
+  height: "400px",
+  border: "1px solid rgb(0 0 0 / 0.8)",
+  borderRadius: "8px",
+  marginBottom: "2rem"
+});
+
+const FloatingCardRight = styled("div", {
+  "--rotate-deg": "0deg",
+  "--rotate-deg-hover": "0deg",
+  position: "absolute",
+  backgroundColor: "#ffffff",
+  color: "#262626", 
+  right: "-25px",
+  bottom: "-20px",
+  maxWidth: "325px",
+  borderRadius: "6px",
+  padding: "6px 10px",
+  border: "1px solid rgb(0 0 0 / 0.2)",
+  boxShadow: "0px 0px 4px rgba(15, 15, 15, 0.02), 0px 0px 3px rgba(15, 15, 15, 0.08), 0px 0px 3px rgba(15, 15, 15, 0.13), 0px 0px 1px rgba(15, 15, 15, 0.15)",
+  transform: "rotate(var(--rotate-deg))",
+  transition: "0.25s ease-in-out",
+  "&:hover": {
+    transform: "rotate(var(--rotate-deg-hover)) translate(15px, 5px)",
+  }
+});
+
+const FloatingCardLeft = styled("div", {
+  "--rotate-deg": "0deg",
+  "--rotate-deg-hover": "0deg",
+  position: "absolute",
+  backgroundColor: "#ffffff",
+  color: "#262626", 
+  left: "-20px",
+  bottom: "-20px",
+  maxWidth: "325px",
+  borderRadius: "6px",
+  padding: "6px 10px",
+  border: "1px solid rgb(0 0 0 / 0.2)",
+  boxShadow: "0px 0px 4px rgba(15, 15, 15, 0.02), 0px 0px 3px rgba(15, 15, 15, 0.08), 0px 0px 3px rgba(15, 15, 15, 0.13), 0px 0px 1px rgba(15, 15, 15, 0.15)",
+  transform: "rotate(var(--rotate-deg))",
+  transition: "0.25s ease-in-out",
+  "&:hover": {
+    transform: "rotate(var(--rotate-deg-hover)) translate(-15px, -5px)",
+  }
+});
+
+const CardTitle = styled("div", {
+  fontFamily: "var(--dm-sans)",
+  fontWeight: 500,
+  fontSize: "1.15rem",
+  color: "$gray1"
+});
+
+const CardDescription = styled("div", {
+  fontFamily: "var(--dm-sans)",
+  fontWeight: 500,
+  fontSize: "0.8rem",
+  color: "$gray9"
 });
 
 const Row = styled("div", {
   display: "flex",
   flexWrap: "wrap",
-});
-
-const Column = styled("div", {
-  display: "flex",
-  flexDirection: "column",
-  gap: "1rem",
-});
-
-const YearTitle = styled("h1", {
-  fontFamily: "var(--dm-sans)",
-  fontWeight: 500,
-  fontSize: "1.25rem",
-  color: "$white",
-});
-
-const YearDiv = styled("div", {
-  display: "flex",
-  justifyContent: "center",
-  margin: "5px",
 });
